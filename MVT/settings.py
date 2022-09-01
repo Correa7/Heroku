b95f10443b1623434c8c46a5a3f3e199dfdc0023
Correa7 <correadamian2019@gmail.com>
2022-09-01 01:44:07 -0300

    requirements

--- MVT/settings.py
+++ MVT/settings.py
@@ -113,10 +113,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'Media/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR/"static",]
+    BASE_DIR/"Media/static",]
 
 
 
